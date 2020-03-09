c145db29cde161a140ea595a4c3a1da5d291361c
Luiz Gabriel <byeh997@gmail.com>
2020-03-09 11:56:49 +0000

    iniciodas atualizações

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+class bloco:
+    v1 = []
+    v2 = []
+    v3 = []
+
+    def init(self):
+        self.v1.append('a')
+        self.v1.append('b')
+        self.v1.append('c')
+        return random.shuffle(v1)
+    def poss(v1, v2, v3):
+
+
+class main:
+    total = []
+    bloco.init(bloco.v1)
+    print(bloco.v1)
+
