acc81c20350d3ae311e2975176f3bc41539ef5b8
Luiz Gabriel <byeh997@gmail.com>
2020-03-16 12:25:27 +0000

    solução funcional

--- main.py
+++ main.py
@@ -7,11 +7,18 @@ class Bloco:
     v3 = []
 
     def init(self, pai = None):
-        self.v1.append('a')
-        self.v1.append('b')
-        self.v1.append('c')
         self.pai = pai
-        return random.shuffle(self.v1)
+        initializer = ['a', 'b', 'c']
+        random.shuffle(initializer)
+        while initializer:
+            i = random.randint(0,2)
+            if(i == 0):
+                self.v1.append(initializer.pop())
+            elif i == 1:
+                self.v2.append(initializer.pop())
+            elif i == 2:
+                self.v3.append(initializer.pop())
+        
 
     def poss(self):
         f = []
@@ -61,29 +68,29 @@ class Bloco:
             f.append(f1)
             f.append(f2)
         elif(len(self.v1) == 2):
-            if(len(self.v2 == 1)):
+            if(len(self.v2) == 1):
                 f1 = Bloco()
                 f1.pai = self
-                f1.v1 = self.v1
-                f1.v2 = self.v2
-                f1.v3 = self.v3
+                f1.v1 = self.v1.copy()
+                f1.v2 = self.v2.copy()
+                f1.v3 = self.v3.copy()
                 f1.v1.append(f1.v2.pop())
                 f2 = Bloco()
-                f2.v1 = self.v1
-                f2.v2 = self.v2
-                f2.v3 = self.v3
+                f2.v1 = self.v1.copy()
+                f2.v2 = self.v2.copy()
+                f2.v3 = self.v3.copy()
                 f2.v2.append(f2.v1.pop())
                 f3 = Bloco()
                 f3.pai = self
-                f3.v1 = self.v1
-                f3.v2 = self.v2
-                f3.v3 = self.v3
+                f3.v1 = self.v1.copy()
+                f3.v2 = self.v2.copy()
+                f3.v3 = self.v3.copy()
                 f3.v3.append(f3.v2.pop())
                 f4 = Bloco()
                 f4.pai = self
-                f4.v1 = self.v1
-                f4.v2 = self.v2
-                f4.v3 = self.v3
+                f4.v1 = self.v1.copy()
+                f4.v2 = self.v2.copy()
+                f4.v3 = self.v3.copy()
                 f4.v3.append(f4.v1.pop())
                 f.append(f1)
                 f.append(f2)
@@ -92,56 +99,56 @@ class Bloco:
             else:
                 f1 = Bloco()
                 f1.pai = self
-                f1.v1 = self.v1
-                f1.v2 = self.v2
-                f1.v3 = self.v3
+                f1.v1 = self.v1.copy()
+                f1.v2 = self.v2.copy()
+                f1.v3 = self.v3.copy()
                 f1.v2.append(f1.v3.pop())
                 f2 = Bloco()
                 f2.pai = self
-                f2.v1 = self.v1
-                f2.v2 = self.v2
-                f2.v3 = self.v3
+                f2.v1 = self.v1.copy()
+                f2.v2 = self.v2.copy()
+                f2.v3 = self.v3.copy()
                 f2.v1.append(f2.v3.pop())
                 f3 = Bloco()
                 f3.pai = self
-                f3.v1 = self.v1
-                f3.v2 = self.v2
-                f3.v3 = self.v3
+                f3.v1 = self.v1.copy()
+                f3.v2 = self.v2.copy()
+                f3.v3 = self.v3.copy()
                 f3.v2.append(f3.v1.pop())
                 f4 = Bloco()
                 f4.pai = self
-                f4.v1 = self.v1
-                f4.v2 = self.v2
-                f4.v3 = self.v3
+                f4.v1 = self.v1.copy()
+                f4.v2 = self.v2.copy()
+                f4.v3 = self.v3.copy()
                 f4.v3.append(f4.v1.pop())
                 f.append(f1)
                 f.append(f2)
                 f.append(f3)
                 f.append(f4)
         elif(len(self.v2) == 2):
-            if(len(self.v1 == 1)):
+            if(len(self.v1) == 1):
                 f1 = Bloco()
                 f1.pai = self
-                f1.v1 = self.v1
-                f1.v2 = self.v2
-                f1.v3 = self.v3
+                f1.v1 = self.v1.copy()
+                f1.v2 = self.v2.copy()
+                f1.v3 = self.v3.copy()
                 f1.v1.append(f1.v2.pop())
                 f2 = Bloco()
-                f2.v1 = self.v1
-                f2.v2 = self.v2
-                f2.v3 = self.v3
+                f2.v1 = self.v1.copy()
+                f2.v2 = self.v2.copy()
+                f2.v3 = self.v3.copy()
                 f2.v3.append(f2.v2.pop())
                 f3 = Bloco()
                 f3.pai = self
-                f3.v1 = self.v1
-                f3.v2 = self.v2
-                f3.v3 = self.v3
+                f3.v1 = self.v1.copy()
+                f3.v2 = self.v2.copy()
+                f3.v3 = self.v3.copy()
                 f3.v3.append(f3.v1.pop())
                 f4 = Bloco()
                 f4.pai = self
-                f4.v1 = self.v1
-                f4.v2 = self.v2
-                f4.v3 = self.v3
+                f4.v1 = self.v1.copy()
+                f4.v2 = self.v2.copy()
+                f4.v3 = self.v3.copy()
                 f4.v2.append(f4.v1.pop())
                 f.append(f1)
                 f.append(f2)
@@ -150,56 +157,56 @@ class Bloco:
             else:
                 f1 = Bloco()
                 f1.pai = self
-                f1.v1 = self.v1
-                f1.v2 = self.v2
-                f1.v3 = self.v3
+                f1.v1 = self.v1.copy()
+                f1.v2 = self.v2.copy()
+                f1.v3 = self.v3.copy()
                 f1.v2.append(f1.v3.pop())
                 f2 = Bloco()
                 f2.pai = self
-                f2.v1 = self.v1
-                f2.v2 = self.v2
-                f2.v3 = self.v3
+                f2.v1 = self.v1.copy()
+                f2.v2 = self.v2.copy()
+                f2.v3 = self.v3.copy()
                 f2.v1.append(f2.v3.pop())
                 f3 = Bloco()
                 f3.pai = self
-                f3.v1 = self.v1
-                f3.v2 = self.v2
-                f3.v3 = self.v3
+                f3.v1 = self.v1.copy()
+                f3.v2 = self.v2.copy()
+                f3.v3 = self.v3.copy()
                 f3.v3.append(f3.v2.pop())
                 f4 = Bloco()
                 f4.pai = self
-                f4.v1 = self.v1
-                f4.v2 = self.v2
-                f4.v3 = self.v3
+                f4.v1 = self.v1.copy()
+                f4.v2 = self.v2.copy()
+                f4.v3 = self.v3.copy()
                 f4.v1.append(f4.v2.pop())
                 f.append(f1)
                 f.append(f2)
                 f.append(f3)
                 f.append(f4)
         elif(len(self.v3) == 2):
-            if(len(self.v2 == 1)):
+            if(len(self.v2) == 1):
                 f1 = Bloco()
                 f1.pai = self
-                f1.v1 = self.v1
-                f1.v2 = self.v2
-                f1.v3 = self.v3
+                f1.v1 = self.v1.copy()
+                f1.v2 = self.v2.copy()
+                f1.v3 = self.v3.copy()
                 f1.v1.append(f1.v2.pop())
                 f2 = Bloco()
-                f2.v1 = self.v1
-                f2.v2 = self.v2
-                f2.v3 = self.v3
+                f2.v1 = self.v1.copy()
+                f2.v2 = self.v2.copy()
+                f2.v3 = self.v3.copy()
                 f2.v3.append(f2.v2.pop())
                 f3 = Bloco()
                 f3.pai = self
-                f3.v1 = self.v1
-                f3.v2 = self.v2
-                f3.v3 = self.v3
+                f3.v1 = self.v1.copy()
+                f3.v2 = self.v2.copy()
+                f3.v3 = self.v3.copy()
                 f3.v1.append(f3.v3.pop())
                 f4 = Bloco()
                 f4.pai = self
-                f4.v1 = self.v1
-                f4.v2 = self.v2
-                f4.v3 = self.v3
+                f4.v1 = self.v1.copy()
+                f4.v2 = self.v2.copy()
+                f4.v3 = self.v3.copy()
                 f4.v2.append(f4.v3.pop())
                 f.append(f1)
                 f.append(f2)
@@ -208,27 +215,27 @@ class Bloco:
             else:
                 f1 = Bloco()
                 f1.pai = self
-                f1.v1 = self.v1
-                f1.v2 = self.v2
-                f1.v3 = self.v3
+                f1.v1 = self.v1.copy()
+                f1.v2 = self.v2.copy()
+                f1.v3 = self.v3.copy()
                 f1.v2.append(f1.v3.pop())
                 f2 = Bloco()
                 f2.pai = self
-                f2.v1 = self.v1
-                f2.v2 = self.v2
-                f2.v3 = self.v3
+                f2.v1 = self.v1.copy()
+                f2.v2 = self.v2.copy()
+                f2.v3 = self.v3.copy()
                 f2.v1.append(f2.v3.pop())
                 f3 = Bloco()
                 f3.pai = self
-                f3.v1 = self.v1
-                f3.v2 = self.v2
-                f3.v3 = self.v3
+                f3.v1 = self.v1.copy()
+                f3.v2 = self.v2.copy()
+                f3.v3 = self.v3.copy()
                 f3.v2.append(f3.v1.pop())
                 f4 = Bloco()
                 f4.pai = self
-                f4.v1 = self.v1
-                f4.v2 = self.v2
-                f4.v3 = self.v3
+                f4.v1 = self.v1.copy()
+                f4.v2 = self.v2.copy()
+                f4.v3 = self.v3.copy()
                 f4.v3.append(f4.v1.pop())
                 f.append(f1)
                 f.append(f2)
@@ -237,40 +244,47 @@ class Bloco:
         elif len(self.v1) == 1 and len(self.v2) == 1 and len(self.v3) == 1:
             f1 = Bloco()
             f1.pai = self
-            f1.v1 = self.v1
-            f1.v2 = self.v2
-            f1.v3 = self.v3
-            f1.v1.append(f1.v2.pop())
+            f1.v1 = self.v1.copy()
+            f1.v2 = self.v2.copy()
+            f1.v3 = self.v3.copy()
+
             f2 = Bloco()
             f2.pai = self
-            f2.v1 = self.v1
-            f2.v2 = self.v2
-            f2.v3 = self.v3
-            f2.v1.append(f2.v3.pop())
+            f2.v1 = self.v1.copy()
+            f2.v2 = self.v2.copy()
+            f2.v3 = self.v3.copy()
+            
             f3 = Bloco()
             f3.pai = self
-            f3.v1 = self.v1
-            f3.v2 = self.v2
-            f3.v3 = self.v3
-            f3.v2.append(f3.v1.pop())
+            f3.v1 = self.v1.copy()
+            f3.v2 = self.v2.copy()
+            f3.v3 = self.v3.copy()
+
             f4 = Bloco()
             f4.pai = self
-            f4.v1 = self.v1
-            f4.v2 = self.v2
-            f4.v3 = self.v3
-            f4.v2.append(f4.v3.pop())
+            f4.v1 = self.v1.copy()
+            f4.v2 = self.v2.copy()
+            f4.v3 = self.v3.copy()
+
             f5 = Bloco()
             f5.pai = self
-            f5.v1 = self.v1
-            f5.v2 = self.v2
-            f5.v3 = self.v3
-            f5.v3.append(f5.v1.pop())
+            f5.v1 = self.v1.copy()
+            f5.v2 = self.v2.copy()
+            f5.v3 = self.v3.copy()
+
             f6 = Bloco()
             f6.pai = self
-            f6.v1 = self.v1
-            f6.v2 = self.v2
-            f6.v3 = self.v3
+            f6.v1 = self.v1.copy()
+            f6.v2 = self.v2.copy()
+            f6.v3 = self.v3.copy()
+
+            f1.v1.append(f1.v2.pop())
+            f2.v1.append(f2.v3.pop())
+            f3.v2.append(f3.v1.pop())
+            f4.v2.append(f4.v3.pop())
+            f5.v3.append(f5.v1.pop())
             f6.v3.append(f6.v2.pop())
+
             f.append(f1)
             f.append(f2)
             f.append(f3)
@@ -286,7 +300,7 @@ bl = Bloco()
 bl.init()
 total.append(bl)
 
-while(total[0].v1 != final or total[0].v2 != final or total[0].v3 != final):
+while not(total[0].v1 == final or total[0].v2 == final or total[0].v3 == final):
     posss = total[0].poss()
     for i in posss:
         k = 0
@@ -300,8 +314,8 @@ while(total[0].v1 != final or total[0].v2 != final or total[0].v3 != final):
 
     
 finalmente = []
-finalmente.append[total[0]]
+finalmente.append(total[0])
 while(finalmente[len(finalmente)-1].pai != None):
-    finalmente.append(finalmente[len(finalmente)].pai)
-for i in reversed(finalmente):
-    print(i)
+    finalmente.append(finalmente[len(finalmente)-1].pai)
+for i in finalmente:
+    print(f'p1: {i.v1} - p2: {i.v2} - p3: {i.v3}')
